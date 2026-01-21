5023efe149c0b72360d12de03ca22a67e2e2b669
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-01-21 19:03:08 +0100

    fix: opensourcetree application installation

--- Formula/opensourcetree.rb
+++ Formula/opensourcetree.rb
@@ -27,8 +27,30 @@ class Opensourcetree < Formula
       SH
     else
       bin.install "opensourcetree"
-      (share/"applications").install "opensourcetree.desktop"
-      (share/"icons/hicolor/scalable/apps").install "opensourcetree.svg"
+
+      # Install .desktop file to ~/.local/share/applications for desktop integration
+      local_apps = Pathname.new(Dir.home)/".local/share/applications"
+      local_apps.mkpath
+      desktop_file = local_apps/"opensourcetree.desktop"
+      desktop_file.write <<~DESKTOP
+        [Desktop Entry]
+        Name=OpenSourceTree
+        GenericName=Git Client
+        Comment=Cross-platform Git GUI application
+        Exec=#{bin}/opensourcetree %F
+        Icon=opensourcetree
+        Terminal=false
+        Type=Application
+        Categories=Development;RevisionControl;
+        Keywords=git;version control;vcs;repository;
+        StartupWMClass=OpenSourceTree
+        MimeType=application/x-git;
+      DESKTOP
+
+      # Install icon to ~/.local/share/icons for desktop integration
+      local_icons = Pathname.new(Dir.home)/".local/share/icons/hicolor/scalable/apps"
+      local_icons.mkpath
+      (local_icons/"opensourcetree.svg").write(File.read("opensourcetree.svg"))
     end
   end
 
@@ -40,6 +62,24 @@ class Opensourcetree < Formula
 
         Then it will appear in Spotlight search.
       EOS
+    else
+      <<~EOS
+        Desktop integration files have been installed to:
+          ~/.local/share/applications/opensourcetree.desktop
+          ~/.local/share/icons/hicolor/scalable/apps/opensourcetree.svg
+
+        You may need to log out and back in for the app to appear in your launcher.
+      EOS
+    end
+  end
+
+  def post_uninstall
+    if OS.linux?
+      # Clean up desktop integration files
+      desktop_file = Pathname.new(Dir.home)/".local/share/applications/opensourcetree.desktop"
+      icon_file = Pathname.new(Dir.home)/".local/share/icons/hicolor/scalable/apps/opensourcetree.svg"
+      desktop_file.unlink if desktop_file.exist?
+      icon_file.unlink if icon_file.exist?
     end
   end
 
